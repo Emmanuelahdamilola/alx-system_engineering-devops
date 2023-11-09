4667e471a240d49ace956e6819754c3cbbef0590
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-11-09 11:44:12 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,11 @@
-# Install flask v 2.1.0
-package { 'flask':
-  ensure   => '2.1.0',
+# Install Flask and Werkzeug using pip
+package { 'python3-pip':
+  ensure => 'installed',
 }
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  path    => '/usr/bin',
+  require => Package['python3-pip'],
+}
+
