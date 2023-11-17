24a90d4cb932de9b4cfe8a49bdf7f446d5a7b74f
Emmanuelahdamilola <emmanuelahdamilola1992@gmail.com>
2023-11-17 03:53:51 -0800

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,54 +1,24 @@
-# Puppet manifest for configuring custom HTTP header in Nginx
+# Use Puppet to automate the task of creating a custom HTTP header response
+
+# Execute 'apt-get update' before installing Nginx
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
 
 # Install Nginx package
 package { 'nginx':
-  ensure => installed,
+  ensure => 'present',
 }
 
-# Define Nginx configuration file
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "
-    server {
-      listen 80 default_server;
-      error_page 404 /404.html;
-      location = /404.html {
-        root /var/www/html;
-        internal;
-      }
-      listen [::]:80 default_server;
-
-      root /etc/nginx/html;
-      index index.html index.htm index.nginx-debian.html;
-
-      server_name _;
-      rewrite ^/redirect_me https://www.google.com/ permanent;
-      error_page 404 /404.html;
-      location = /404.html {
-        root /etc/nginx/html;
-        internal;
-      }
-      rewrite ^/redirect_me https://www.google.com permanent;
-
-      # Puppet configuration for custom HTTP header
-      add_header X-Served-By $hostname;
-
-    }
-  ",
-  notify  => Service['nginx'],
+# Add custom HTTP header to Nginx configuration
+file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
 
-# Enable the default site by creating a symbolic link
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  notify  => Service['nginx'],
-}
-
-# Ensure Nginx service is running
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+# Restart Nginx service after updating configuration
+exec { 'run':
+  command => '/usr/sbin/service nginx restart',
 }
 
